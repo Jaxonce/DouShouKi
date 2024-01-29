7667d405296d572151e28829ec63688e5879bd6a
malanone <Maxence.LANONE@etu.uca.fr>
2024-01-29 22:34:19 +0100

    :construction: add class Player, HumanPlayer and RandomPlayer

---
+++ Model/Sources/Model/Class/HumanPlayer.swift
@@ -0,0 +1,14 @@
+//
+//  HumanPlayer.swift
+//  
+//
+//  Created by Maxence Lanone on 29/01/2024.
+//
+
+import Foundation
+
+public class HumanPlayer: Player {
+//    public override init?(withName name: String, andId id: Owner, andInputMethod:) {
+//        
+//    }
+}
---
+++ Model/Sources/Model/Class/RandomPlayer.swift
@@ -0,0 +1,18 @@
+//
+//  RandomPlayer.swift
+//  
+//
+//  Created by Maxence Lanone on 29/01/2024.
+//
+
+import Foundation
+
+public class RandomPlayer: Player{
+    public override init?(withName name: String, andId id: Owner) {
+        super.init(withName: name, andId: id)
+    }
+    
+    public override func chooseMove(in board: Board, with rules: Rules) {
+        
+    }
+}
